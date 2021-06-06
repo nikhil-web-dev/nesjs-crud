94c159b692159a9a086c28bb30f520d4dd5dcf4b
Nikhil <dev.nikhil@hotmail.com>
2021-06-06 13:29:07 +0530

    CRUD operation complete

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Delete } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Posts } from './types/posts';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +8,30 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/create')
+  createPost(@Body() data: Posts) {
+    return this.appService.createPost(data);
+  }
+
+  @Get('/posts')
+  retrievePost(): any {
+    return this.appService.retrievePost();
+  }
+
+  @Get('/post')
+  retrievePostById(@Query('id') id: string) {
+    return this.appService.retreivePostById(id);
+  }
+
+  @Post('/update')
+  update(@Query('id') id: string, @Body() data: any) {
+    return this.appService.update(id, data);
+  }
+
+  @Delete('/delete')
+  delete(@Query('id') id: string) {
+    return this.appService.delete(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { Posts } from './types/posts';
+
+const posts: any = [];
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createPost(data) {
+    const uuid = uuidv4();
+
+    const payload = {
+      id: uuid,
+      title: data.title,
+      description: data.description,
+    };
+
+    return posts.push(payload);
+  }
+
+  retrievePost() {
+    return posts;
+  }
+
+  retreivePostById(data: string) {
+    console.log(data);
+    return posts.find((post) => post.id === data);
+  }
+
+  update(id: string, data: any) {
+    console.log(data);
+    const { title, description } = data;
+
+    const post = posts.find((post) => post.id === id);
+    console.log(post);
+
+    if (title) post.title = title;
+
+    if (description) post.description = description;
+
+    return posts;
+  }
+
+  delete(id: string) {
+    const post = posts.findIndex((post) => post.id === id);
+
+    posts.splice(post, 1);
+
+    return posts;
   }
 }
